c2943da26141a5f21ebb123aaeba0c45ee8d8d77
Facu Ortega <fortega@finnegans.com.ar>
2024-05-20 11:21:57 -0300

    se vuelve a agregar hsqldb para el correcto funcionamiento de los tests

--- src/test/java/com/tallerwebi/integracion/config/HibernateTestConfig.java
+++ src/test/java/com/tallerwebi/integracion/config/HibernateTestConfig.java
@@ -2,7 +2,6 @@ package com.tallerwebi.integracion.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -18,10 +17,10 @@ public class HibernateTestConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/taller_web");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem:db_");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
         return dataSource;
     }
 
@@ -36,12 +35,12 @@ public class HibernateTestConfig {
 
     @Bean
     public HibernateTransactionManager transactionManager(DataSource dataSource) {
-        return new HibernateTransactionManager(sessionFactory(dataSource).getObject());
+        return new HibernateTransactionManager(sessionFactory(dataSource()).getObject());
     }
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create");
