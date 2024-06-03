3411d0518d92d1f6d6761bb495434cf2cd7bf7f3
Facu Ortega <fortega@finnegans.com.ar>
2024-06-02 21:04:19 -0300

    se arregla bug de pagina en blanco

--- src/main/java/com/tallerwebi/presentacion/ControladorDatos.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorDatos.java
@@ -12,20 +12,17 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ControladorDatos {
 
     private final ServicioUsuario servicioUsuario;
     private final ServicioPeso servicioPeso;
-
     private final ServicioMembresia servicioMembresia;
 
     @Autowired
-    public ControladorDatos(ServicioUsuario servicioUsuario,ServicioPeso servicioPeso, ServicioMembresia servicioMembresia){
+    public ControladorDatos(ServicioUsuario servicioUsuario, ServicioPeso servicioPeso, ServicioMembresia servicioMembresia) {
         this.servicioUsuario = servicioUsuario;
         this.servicioPeso = servicioPeso;
         this.servicioMembresia = servicioMembresia;
@@ -33,41 +30,38 @@ public class ControladorDatos {
 
     @Transactional
     @RequestMapping("/datos")
-    public ModelAndView MostrarDatos(HttpServletRequest request) {
+    public ModelAndView mostrarDatos(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-
         ModelMap modelo = new ModelMap();
 
-        if (session != null && session.getAttribute("Email") != null){
-            modelo.put("Email",session.getAttribute("Email") );
-            modelo.put("id",session.getAttribute("id") );
-            Usuario usuario = servicioUsuario.buscarPorId((Long)session.getAttribute("id"));
+        if (session != null && session.getAttribute("Email") != null) {
+            modelo.put("Email", session.getAttribute("Email"));
+            modelo.put("id", session.getAttribute("id"));
+            Usuario usuario = servicioUsuario.buscarPorId((Long) session.getAttribute("id"));
             ArrayList pesosRegistro = servicioPeso.obtenerPesosPorMes(usuario.getId());
             Membresia membresia = servicioMembresia.membresiasPorId(usuario.getId()).get(0);
-            modelo.put("registroPeso",pesosRegistro);
-            modelo.put("usuario",usuario);
-            modelo.put("membresia",membresia);
+            modelo.put("registroPeso", pesosRegistro);
+            modelo.put("usuario", usuario);
+            modelo.put("membresia", membresia);
             modelo.put("datosPeso", new DatosPeso());
             return new ModelAndView("datos", modelo);
         }
 
-        return null;
+        return new ModelAndView("redirect:/login");
     }
 
     @Transactional
-    @RequestMapping("/cargar-peso")
-    public ModelAndView PostPeso(@ModelAttribute("datosPeso") DatosPeso datosPeso,HttpServletRequest request) {
-        ModelMap modelo = new ModelMap();
+    @RequestMapping(value = "/cargar-peso", method = RequestMethod.POST)
+    public ModelAndView postPeso(@ModelAttribute("datosPeso") DatosPeso datosPeso, HttpServletRequest request) {
         HttpSession session = request.getSession(false);
 
-        if (session != null && session.getAttribute("Email") != null){
-            modelo.put("id",session.getAttribute("id") );
-            Usuario usuario = servicioUsuario.buscarPorId((Long)session.getAttribute("id"));
-            servicioPeso.postPeso(usuario,datosPeso);
-            return new ModelAndView("redirect:/datos", modelo);
+        if (session != null && session.getAttribute("Email") != null) {
+            Long usuarioId = (Long) session.getAttribute("id");
+            Usuario usuario = servicioUsuario.buscarPorId(usuarioId);
+            servicioPeso.postPeso(usuario, datosPeso);
+            return new ModelAndView("redirect:/datos?id=" + usuarioId);
         }
-        return null;
+
+        return new ModelAndView("redirect:/login");  
     }
-
-
 }
